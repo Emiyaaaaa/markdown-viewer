d51aee6fe9b6f5dfd8f323138bf213a5ad3bcc20
emiya <emiya@emiya.com.cn>
2024-05-21 02:32:06 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import "github-markdown-css/github-markdown.css";
 import { MDXRemote } from "next-mdx-remote/rsc";
-import { useEffect, useState } from "react";
 
 export default async function Home(props: { searchParams: { url?: string } }) {
 	const res = await fetch(props.searchParams.url!);
 	const markdown = await res.text();
 
-	return <MDXRemote source={markdown} />;
+	return (
+		<main className="markdown-body">
+			<MDXRemote source={markdown} />
+		</main>
+	);
 }
