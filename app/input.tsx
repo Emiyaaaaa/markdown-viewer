bbd427d4d97f25e6358d2580706fa70f48b398cf
lihaozheng001 <lihaozheng001@realsee.com>
2024-05-21 20:02:40 +0800

    update style

--- app/input.tsx
+++ app/input.tsx
@@ -5,11 +5,14 @@ export default function Input() {
   const [value, setValue] = useState("");
 
 	return (
-    <main className="input">
-		  <input className="" onChange={e => setValue(e.target.value)} />
+    <main className="input-container">
+      <div className="input-wrapper">
+		  <input placeholder="https://example.md" autoFocus className="" onChange={e => setValue(e.target.value)} />
       <button>
-        <a href={`/?url=${value}`}>get</a>
+        <a href={`/?url=${value}`}>GO</a>
       </button>
+      </div>
+      <desc>Input a online markdown file uri, get a online preview markdown link.</desc>
     </main>
 	);
 }
