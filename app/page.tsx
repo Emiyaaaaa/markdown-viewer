b0c3ef6e564a95b53632c94406ac92569305d5f3
lihaozheng001 <lihaozheng001@realsee.com>
2024-11-18 17:23:21 +0800

    fix: fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import Input from "./modules/input";
 import Markdown from "./modules/markdown";
 
-export default async function Home(props: { searchParams: { url?: string } }) {
-	const url = props.searchParams?.url;
+export default async function Home(props: { searchParams: Promise<{ url?: string }> }) {
+	const url = (await props.searchParams)?.url;
 
 	return url ? <Markdown url={url} /> : <Input />;
 }
